e741315f2046d0b15670a101f6817ef60ac8f952
rmzNadir <dgzrz99@gmail.com>
2021-08-03 17:36:52 -0500

    ✨

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const withTM = require('next-transpile-modules')([
+  'remark',
+  'unified',
+  'bail',
+  'is-plain-obj',
+  'trough',
+  'vfile',
+  'unist-util-stringify-position',
+  'mdast-util-to-string',
+  'micromark-util-combine-extensions',
+  'micromark-util-symbol/constants.js',
+  'micromark-util-resolve-all',
+  'parse-entities/decode-entity.js',
+  'character-entities',
+  'micromark-util-html-tag-name',
+  'mdast-util-to-markdown',
+  'zwitch',
+  'longest-streak',
+]);
+
+module.exports = withTM();
